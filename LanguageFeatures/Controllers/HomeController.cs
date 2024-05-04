82a254a1eb699372a1ca6b15f525264b16621b78
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-05-04 15:58:17 -0300

    Learning about null state analysis

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product?[] products = Product.GetProducts();
+            
+            return View(new string[] { products[2]?.Name ?? "No Value" });
+
+            //return View(new string[] { products[2]!.Name });
+
+            //string? val = products[2]?.Name;
+            //if (val != null)
+            //{
+            //    return View(new string[] { val });
+            //}
+            //return View(new string[] { "No Value" });
+        }
+    }
+}
