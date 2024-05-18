1fe7109e0f8345f8c0a8bd59eb1ed0e62cb9cbaf
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-05-18 16:29:53 -0300

    Fininshin the learnings of the chapter 5

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -111,6 +111,18 @@ namespace LanguageFeatures.Controllers
             }
             return View(output);
         }
+
+        public ViewResult Index9()
+        {
+            var products = new[] {
+                 new { Name = "Kayak", Price = 275M },
+                 new { Name = "Lifejacket", Price = 48.95M },
+                 new { Name = "Soccer ball", Price = 19.50M },
+                 new { Name = "Corner flag", Price = 34.95M }
+            };
+            return View(products.Select(p =>
+            $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
+        }
     }
 }
  
