c9e50afae0dcc30000d5a43cf492c4d63b5a7608
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-05-18 16:43:04 -0300

    Etarting Chapter 6 - Testing

---
+++ Testing/SimpleApp/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
+
+app.Run();
