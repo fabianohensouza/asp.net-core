6ce102527cf32b3ce753743524536738e233e16f
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-05-20 07:51:06 -0300

    Starting with unit tests using XUnit Framework

---
+++ Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Controllers;
+using SimpleApp.Models;
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            // Arrange
+            var controller = new HomeController();
+            Product[] products = new Product[] {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M}
+            };
+
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model
+            as IEnumerable<Product>;
+            // Assert
+            Assert.Equal(products, model,
+            Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name
+            && p1?.Price == p2?.Price));
+        }
+    }
+}
