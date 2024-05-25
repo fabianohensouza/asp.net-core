fd1d8d4fa849c9e5fbd24bb2bb224514859ede82
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-05-25 10:49:28 -0300

    Configuring database EF Core

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
 
 var app = builder.Build();
 
