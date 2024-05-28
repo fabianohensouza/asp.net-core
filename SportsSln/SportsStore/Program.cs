7ffba489110370513e4eb8fc09e5a2d54e759f88
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-05-28 08:14:48 -0300

    Initializing the database

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -9,11 +9,15 @@ builder.Services.AddDbContext<StoreDbContext>(opts => {
     builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+
 app.MapDefaultControllerRoute();
 
 app.Run();
