3cafdbe934718c21f488cafb98b2ad2297104f89
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-05-30 12:32:57 -0300

    Creating tag helpers

--- SportsSln/SportsStore.Tests/HomeControllerTests.cs
+++ SportsSln/SportsStore.Tests/HomeControllerTests.cs
@@ -30,5 +30,32 @@ namespace SportsStore.Tests
             Assert.Equal("P1", prodArray[0].Name);
             Assert.Equal("P2", prodArray[1].Name);
         }
+
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"},
+                new Product {ProductID = 6, Name = "P6"}
+            }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            IEnumerable<Product> result = (controller.Index(2) as ViewResult)?.ViewData.Model
+            as IEnumerable<Product> ?? Enumerable.Empty<Product>();
+
+            // Assert
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 3);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
     }
 }
--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -6,12 +6,20 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index() => View(repository.Products);
+
+        //public IActionResult Index() => View(repository.Products);
         //Expression bodied function
+
+        public ViewResult Index(int productPage = 1) 
+            => View(repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
     }
 }
