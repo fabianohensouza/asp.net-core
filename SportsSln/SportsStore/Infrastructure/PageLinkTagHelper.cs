d214a339eec758c64f72b9981eefea25009db692
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-06-03 07:39:45 -0300

    Styling the web page with Bootstrap and partial views

--- SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -23,6 +23,11 @@ namespace SportsStore.Infrastructure
         public PagingInfo? PageModel { get; set; }
         public string? PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -37,7 +42,14 @@ namespace SportsStore.Infrastructure
                     new { productPage = i });
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
-                    if (i < PageModel.TotalPages) result.InnerHtml.AppendHtml(" - ");
+                    //if (i < PageModel.TotalPages) result.InnerHtml.AppendHtml(" - ");
+
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
+
                 }
 
                 output.Content.AppendHtml(result.InnerHtml);
