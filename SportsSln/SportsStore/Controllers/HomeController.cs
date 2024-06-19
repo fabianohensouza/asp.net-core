8133f22f1b1dc65c84adacbd3e4b54e4c5d0546e
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-06-19 07:50:27 -0300

    Reviewing the execution of the code

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -29,7 +29,6 @@ namespace SportsStore.Controllers
                 {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
-                    //TotalItems = repository.Products.Count()
                     TotalItems = (category == null) ? repository.Products.Count()
                     : repository.Products.Where(e => e.Category == category).Count()
                 },
