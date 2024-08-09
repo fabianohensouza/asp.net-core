c2cba95f9e5c9e3e9c69a97ddf9d2c2aa425b3b1
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-08-09 07:33:42 -0300

    Completing the order submition

--- SportsSln/SportsStore/Models/StoreDbContext.cs
+++ SportsSln/SportsStore/Models/StoreDbContext.cs
@@ -10,6 +10,5 @@ namespace SportsStore.Models
 
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Order> Orders => Set<Order>();
-        public DbSet<CartLine> CartLines => Set<CartLine>();
     }
 }
