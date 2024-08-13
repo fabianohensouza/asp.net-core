88bf06c02b10b539f2abdbf3d1dee1e56678fcd9
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-08-13 07:46:45 -0300

    Creating the products edition page

--- SportsSln/SportsStore/Models/Repositories/EFStoreRepository.cs
+++ SportsSln/SportsStore/Models/Repositories/EFStoreRepository.cs
@@ -8,5 +8,22 @@
             context = ctx;
         }
         public IQueryable<Product> Products => context.Products;
+
+        public void CreateProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            context.SaveChanges();
+        }
     }
 }
