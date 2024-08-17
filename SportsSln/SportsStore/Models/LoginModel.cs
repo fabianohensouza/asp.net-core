943c32587f23336c96e3bfa66ad377a935ff3e9d
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-08-17 16:00:52 -0300

    Staring with authorization and authentication

---
+++ SportsSln/SportsStore/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
