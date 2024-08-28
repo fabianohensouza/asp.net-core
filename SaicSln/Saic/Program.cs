6759da18b4d6dc3f4da5deac41dafc222c193f0b
Fabiano Souza <33223817+fabianohensouza@users.noreply.github.com>
2024-08-28 07:43:39 -0300

    Configuring the project

--- SaicSln/Saic/Program.cs
+++ SaicSln/Saic/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Saic.Models;
+using Saic.Models.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsProduction())
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseRequestLocalization(opts => {
+    opts.AddSupportedCultures("pt-BR")
+    .AddSupportedUICultures("pt-BR")
+    .SetDefaultCulture("pt-BR");
+});
+
+app.UseStaticFiles();
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+app.MapBlazorHub();
 
 app.Run();
